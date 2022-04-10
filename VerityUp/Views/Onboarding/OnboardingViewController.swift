a622da2174cd79d877c50f72e7dd7fe3ab9e6efe
rivaldofez <rivaldofez@gmail.com>
2022-04-10 23:00:30 +0700

    Initial Commit

---
+++ VerityUp/Views/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingViewController.swift
+//  VerityUp
+//
+//  Created by Rivaldo Fernandes on 10/04/22.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
